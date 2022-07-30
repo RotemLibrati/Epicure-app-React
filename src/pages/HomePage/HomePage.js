d9f5b5039c1ad5e2f5317e53aa20231882ebd7a9
RotemLibrati <rotemlibrati1@gmail.com>
2022-07-30 14:37:54 +0300

    Added Chef of the week and his restaurants

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -4,8 +4,10 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 //Redux
-import { getTopRestaurants } from '../../actions/restaurant.';
+import { getTopRestaurants, getAllRestaurants } from '../../actions/restaurant';
 import { getTopDishes } from '../../actions/dish';
+import { getAllChefs } from '../../actions/chef';
+
 
 //Component
 import Box from '../../components/Box/Box';
@@ -13,13 +15,17 @@ import RestaurantItem from '../../components/Restaurants/RestaurantItem';
 import DishItem from '../../components/Dishes/DishItem';
 import Spinner from '../../components/layout/Spinner';
 import Icons from '../../components/Icons/Icons';
+import ChefOfTheWeek from '../../components/ChefOfTheWeek/ChefOfTheWeek';
 
 
-const HomePage = ({ getTopRestaurants, getTopDishes, restaurant: { top_restaurants, loading }, dish: { top_dishes, loading_d } }) => {
+const HomePage = ({ getTopRestaurants, getTopDishes, getAllRestaurants,
+  getAllChefs, restaurant: { restaurants, top_restaurants, loading_r }, dish: { top_dishes, loading_d }, chef: { chefs, loading_c } }) => {
   useEffect(() => {
     getTopRestaurants();
     getTopDishes();
-  }, [getTopRestaurants, getTopDishes]);
+    getAllChefs();
+    getAllRestaurants();
+  }, [getTopRestaurants, getTopDishes, getAllChefs, getAllRestaurants]);
   return (
     <div>
       <img className='hero-picture' src={require('../../assert/images/hero-picture 1.png')} alt='logo-search' />
@@ -29,7 +35,7 @@ const HomePage = ({ getTopRestaurants, getTopDishes, restaurant: { top_restauran
           <div className='title-pop-rest'>Popular restaurant in epicure:</div>
           <div className='pop-rest-form'>
             <div className='pop-rest'>
-              {loading ? (<div className='spinner'>
+              {loading_r ? (<div className='spinner'>
                 <Spinner />
               </div>
               ) : (
@@ -50,18 +56,26 @@ const HomePage = ({ getTopRestaurants, getTopDishes, restaurant: { top_restauran
         <div className='dishes'>
           <div className='dishes-title'> Signature dish of: </div>
           <div className='form-dishes'>
-              {loading_d ? (<div className='spinner'>
-                <Spinner />
-              </div>
-              ) : (
-                top_dishes.map(dish => (
-                  <DishItem key={dish._id} dish={dish} />
-                ))
-              )}
+            {loading_d ? (<div className='spinner'>
+              <Spinner />
+            </div>
+            ) : (
+              top_dishes.map(dish => (
+                <DishItem key={dish._id} dish={dish} />
+              ))
+            )}
           </div>
         </div>
         <div className='icons'>
           <Icons />
+        </div>
+        <div className='frame-145'>
+          {loading_c ? (<div className='spinner'>
+            <Spinner />
+          </div>
+          ) : (
+            <ChefOfTheWeek key={chefs[0]._id} chef={chefs[0]} restaurants={restaurants}/>
+          )}
         </div>
       </div>
 
@@ -72,13 +86,17 @@ const HomePage = ({ getTopRestaurants, getTopDishes, restaurant: { top_restauran
 HomePage.propTypes = {
   getTopRestaurants: PropTypes.func.isRequired,
   getTopDishes: PropTypes.func.isRequired,
+  getAllChefs: PropTypes.func.isRequired,
+  getAllRestaurants: PropTypes.func.isRequired,
   restaurant: PropTypes.object.isRequired,
-  dish: PropTypes.object.isRequired
+  dish: PropTypes.object.isRequired,
+  chef: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
   restaurant: state.restaurant,
-  dish: state.dish
+  dish: state.dish,
+  chef: state.chef
 });
 
-export default connect(mapStateToProps, { getTopRestaurants, getTopDishes })(HomePage);
+export default connect(mapStateToProps, { getTopRestaurants, getTopDishes, getAllChefs, getAllRestaurants })(HomePage);
