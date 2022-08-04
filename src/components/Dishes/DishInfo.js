52a0f89f715715cf929d3aa546ff7b7ae6677796
RotemLibrati <rotemlibrati1@gmail.com>
2022-08-04 20:57:35 +0300

    Page that need to display the dish info and reservation

---
+++ src/components/Dishes/DishInfo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './DishInfo.css';
+
+const DishInfo = () => {
+    return (
+        <div className='dish-main'>
+            <div className='display-dish-info'>Dish</div>
+        </div>
+    )
+};
+
+export default DishInfo;
