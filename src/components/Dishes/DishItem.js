f94d551f749bf6c5e7c1e937fc686263cfe5cbad
RotemLibrati <rotemlibrati1@gmail.com>
2022-08-04 20:58:00 +0300

    Component that display the dish

--- src/components/Dishes/DishItem.js
+++ src/components/Dishes/DishItem.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import './DishItem.css';
-const DishItem = ({ dish: { name, image, description, price } }) => {
+import { Link } from 'react-router-dom';
+
+
+const DishItem = ({ dish: {_id, name, image, description, price } }) => {
   return (
-    <div className='dish-card'> 
-      <img className='image-dishes' src={image} alt={image} />
-      <div className='dish-info-card'>
-        <div className='dish-name'>{name}</div>
-        <div className='dish-desc'>{description}</div>
-        <div className='dish-price'>
-          <div className='dish-line2-price'></div>
-          <div className='price-form'>
-            <div className='price'>₪{price}</div>
+    <React.Fragment>
+      <Link className='dish-card' to={`/restaurants/dish/${_id}`}>
+        <img className='image-dishes' src={image} alt={image} />
+        <div className='dish-info-card'>
+          <div className='dish-name'>{name}</div>
+          <div className='dish-desc'>{description}</div>
+          <div className='dish-price'>
+            <div className='dish-line2-price'></div>
+            <div className='price-form'>
+              <div className='price'>₪{price}</div>
+            </div>
+            <div className='dish-line1-price'></div>
           </div>
-          <div className='dish-line1-price'></div>
         </div>
-      </div>
-    </div>
+      </Link>
+    </React.Fragment>
   )
 };
 
