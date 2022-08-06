b73af023b02a19d6d1c56c2e13a184dca623817b
RotemLibrati <rotemlibrati1@gmail.com>
2022-08-06 18:38:43 +0300

    Context for orders

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,13 +1,19 @@
-import React, { useEffect } from 'react';
-import './NavBar.css';
+import React, { useEffect, useContext } from 'react';
+import './Navbar.css';
 import { Link, useNavigate } from 'react-router-dom';
+
+import { SetOrder } from '../../context/SetToggleCart';
+
+//Component
+// import Orders from '../Orders/Orders';
 
 
 const Navbar = () => {
+    const { toggleOrder, changeToggleOrder } = useContext(SetOrder);
     const history = useNavigate();
     useEffect(() => {
         history('/')
-    },[]);
+    }, []);
     return (
         <nav className='navbar'>
             <div className='left-side'>
@@ -16,18 +22,20 @@ const Navbar = () => {
                     <a className='a' href='/'>Epicure</a>
                 </h1>
                 <h3 className='h3'>
-                    <Link  className='link' to="/restaurants">Restaurant</Link>
+                    <Link className='link' to="/restaurants">Restaurant</Link>
                 </h3>
                 <h3 className='h3'>
                     <Link className='link' to="/chefs">Chef</Link>
                 </h3>
             </div>
             <div className='center'></div>
+            
             <div className='right-side'>
+            {/* {toggleOrder && <Orders />}     */}
                 <div className='logo'>
                     <img className='logo-right' src={require('../../assert/images/Search.png')} alt='logo-search' />
                     <img className='logo-right' src={require('../../assert/images/Profile.png')} alt='logo-profile' />
-                    <img className='logo-right' src={require('../../assert/images/Cart.png')} alt='logo-cart' />
+                    <img className='logo-right' src={require('../../assert/images/Cart.png')} alt='logo-cart' onClick={() => changeToggleOrder(!toggleOrder)} />
                 </div>
 
             </div>
